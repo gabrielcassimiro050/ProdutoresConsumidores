426531138ff97208e5bf1a3f11026effb7e683ce
gabrielcassimiro050 <163203496+gabrielcassimiro050@users.noreply.github.com>
2025-04-09 16:12:52 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,60 +3,60 @@ using System.Threading.Tasks;
 using System.Collections.Concurrent;    
 
 BlockingCollection <(int pedido, int prato)> pedidos = new BlockingCollection<(int pedido, int prato)>();
+ConcurrentDictionary <string, int> porcoes = new ConcurrentDictionary<string, int>();
 
-var porcoes = new int[4];
-var porcoesString = new string[4];
-
-const int ARROZ = 0, CARNE = 1, MACARRAO = 2, MOLHO = 3;
-
-porcoesString[ARROZ] = "Arroz";
-porcoesString[CARNE] = "Carne";
-porcoesString[MACARRAO] = "Macarrão";
-porcoesString[MOLHO] = "Molho";
+porcoes.TryAdd("Arroz", 0);
+porcoes.TryAdd("Carne", 0);
+porcoes.TryAdd("Macarrao", 0);
+porcoes.TryAdd("Molho", 0);
 
 const int tempoDePreparo = 2000;
 
 object lockConsole = new object();
 
-void ConsoleLock(string msg, ConsoleColor cor)
+void ConsoleLock(string msg)
 {
+    //Console.WriteLine(msg);
     lock (lockConsole)
     {
-        var aux = Console.ForegroundColor;
-        Console.ForegroundColor = cor;
         Console.WriteLine(msg);
-        Console.ForegroundColor = aux;
     }
 }
 
 void GarcomSpeech(string msg){
-    ConsoleLock("                         ", ConsoleColor.Blue);
-    ConsoleLock("              ______     ", ConsoleColor.Blue);
-    ConsoleLock("             /      \\   ", ConsoleColor.Blue);
-    ConsoleLock("            |        |	  ", ConsoleColor.Blue);
-    ConsoleLock("            |        |   ", ConsoleColor.Blue);
-    ConsoleLock("            |        |   ", ConsoleColor.Blue);
-    ConsoleLock("            |        |   "+msg, ConsoleColor.Blue);
-    ConsoleLock("            |        |   ", ConsoleColor.Blue);
-    ConsoleLock("            | o    o |   ", ConsoleColor.Blue);
-    ConsoleLock("           ||___^^___||  ", ConsoleColor.Blue);
-    ConsoleLock("            |___||___|   ", ConsoleColor.Blue);
-    ConsoleLock("              T    T     ", ConsoleColor.Blue);
+    var aux = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("                         ");
+    Console.WriteLine("              ______     ");
+    Console.WriteLine("             /      \\   ");
+    Console.WriteLine("            |        |	");
+    Console.WriteLine("            |        |   ");
+    Console.WriteLine("            |        |   ");
+          ConsoleLock("            |        |   "+msg);
+    Console.WriteLine("            |        |   ");
+    Console.WriteLine("            | o    o |   ");
+    Console.WriteLine("           ||___^^___||  ");
+    Console.WriteLine("            |___||___|   ");
+    Console.WriteLine("              T    T     ");
+    Console.ForegroundColor = aux;
 }
 
-void ChefSpeech(string msg){
-    ConsoleLock("              ______     ", ConsoleColor.Red);
-    ConsoleLock("             / |  | \\   ", ConsoleColor.Red);
-    ConsoleLock("            | |    | |   ", ConsoleColor.Red);
-    ConsoleLock("            | |    | |	  ", ConsoleColor.Red);
-    ConsoleLock("             \\_|__|_/   ", ConsoleColor.Red);
-    ConsoleLock("              |____|     ", ConsoleColor.Red);
-    ConsoleLock("             /      \\   "+msg, ConsoleColor.Red);
-    ConsoleLock("            |        |   ", ConsoleColor.Red);
-    ConsoleLock("            | o    o |   ", ConsoleColor.Red);
-    ConsoleLock("           ||   /\\   || ", ConsoleColor.Red);
-    ConsoleLock("            |________|   ", ConsoleColor.Red);
-    ConsoleLock("              T    T     ", ConsoleColor.Red);
+void ChefSpeech(string msg, ConsoleColor color){
+    var aux = Console.ForegroundColor;
+    Console.ForegroundColor = color;
+    Console.WriteLine("              ______     ");
+    Console.WriteLine("             / |  | \\   "+$"Arroz: {porcoes["Arroz"]}");
+    Console.WriteLine("            | |    | |   "+$"Carne: {porcoes["Carne"]}");
+    Console.WriteLine("            | |    | |	"+$"Macarrão: {porcoes["Macarrao"]}");
+    Console.WriteLine("             \\_|__|_/   "+$"Molho: {porcoes["Molho"]}");
+    Console.WriteLine("              |____|     ");
+          ConsoleLock("             /      \\   "+msg);
+    Console.WriteLine("            |        |   ");
+    Console.WriteLine("            | o    o |   ");
+    Console.WriteLine("           ||   /\\   || ");
+    Console.WriteLine("            |________|   ");
+    Console.WriteLine("              T    T     ");
+    Console.ForegroundColor = aux;
 }
 
 int pedido = 1;
@@ -87,64 +87,91 @@ void Chef(int index){
     
     foreach (var (pedido, prato) in pedidos.GetConsumingEnumerable())
     {
-        ChefSpeech($"[Chef {index}] Iniciando o pedido {pedido} do prato {prato}");
+        ChefSpeech($"[Chef {index}] Iniciando o pedido {pedido} do prato {prato}", ConsoleColor.Red);
         
         prepararPrato(prato, index);
 
-        ChefSpeech($"[Chef {index}] Finalizei o pedido {pedido} do prato {prato}");
+        ChefSpeech($"[Chef {index}] Finalizei o pedido {pedido} do prato {prato}", ConsoleColor.Red);
     }
 }
 
-void prepararComponente(int componente, int nPorcoes, int chef){
-    ChefSpeech($"[Chef {chef}] Preparando {nPorcoes} porções de {porcoesString[componente]}");
-    Thread.Sleep(tempoDePreparo);
+void prepararComponente(string componente, int nPorcoes, int chef){
+    if(porcoes[componente] > 0) return;
     porcoes[componente] += nPorcoes;
-                     
-  
-    ChefSpeech($"[Chef {chef}] Finalizei a produção de {nPorcoes} porções de {porcoesString[componente]}. Estoque: {porcoes[componente]}");
+    ChefSpeech($"[Chef {chef}] Preparando {nPorcoes} porções de "+componente, ConsoleColor.Green);
+
+    Thread.Sleep(tempoDePreparo);
+    
+
+    ChefSpeech($"[Chef {chef}] Finalizei a produção de {nPorcoes} porções de {componente}", ConsoleColor.Green);
 }
 
 void prepararPrato(int prato, int chef){
     switch(prato){
         case 1:
-            if(porcoes[ARROZ]==0){
-                
-                prepararComponente(ARROZ, 3, chef);
-            }
-            if(porcoes[CARNE]==0){
-                prepararComponente(CARNE, 2, chef);
-            }
             Thread.Sleep(2000);
+            if(porcoes["Arroz"]<=0 || porcoes["Arroz"]-1<=0){
+                prepararComponente("Arroz", 3, chef);
+            }
+            if(porcoes["Carne"]<=0 || porcoes["Carne"]-1<=0){
+                prepararComponente("Carne", 2, chef);
+            }
+
+            if(porcoes["Arroz"]>0 && porcoes["Carne"]>0){
+                porcoes["Arroz"]--;
+                porcoes["Carne"]--;
+            }else{
+                prepararPrato(prato, chef);
+            }
+            
         break;
 
         case 2:
-            if(porcoes[MACARRAO]==0){
-                prepararComponente(MOLHO, 4, chef);
-            }
-            if(porcoes[MOLHO]==0){
-                prepararComponente(MOLHO, 2, chef);
-            }
             Thread.Sleep(2000);
+            if(porcoes["Macarrao"]<=0 || porcoes["Macarrao"]-1<=0){
+                prepararComponente("Macarrao", 4, chef);
+            }
+            if(porcoes["Molho"]<=0 || porcoes["Molho"]-1<=0){
+                prepararComponente("Molho", 2, chef);
+            }
+            if(porcoes["Macarrao"]>0 && porcoes["Molho"]>0){
+                porcoes["Macarrao"]--;
+                porcoes["Molho"]--;
+            }else{
+                prepararPrato(prato, chef);
+            }
+
         break;
 
         case 3:
-            if(porcoes[ARROZ]==0){
-                prepararComponente(ARROZ, 3, chef);
+           Thread.Sleep(3000);
+            
+            if(porcoes["Arroz"]<=0 || porcoes["Arroz"]-1<=0){
+                prepararComponente("Arroz", 3, chef);
             }
-            if(porcoes[CARNE]==0){
-                prepararComponente(CARNE, 2, chef);
+            if(porcoes["Carne"]<=0|| porcoes["Carne"]-1<=0){
+                prepararComponente("Carne", 2, chef);
             }
-            if(porcoes[MOLHO]==0){
-                prepararComponente(MOLHO, 2, chef);
+            if(porcoes["Molho"]<=0|| porcoes["Molho"]-1<=0){
+                prepararComponente("Molho", 2, chef);
             }
-            Thread.Sleep(3000);
+
+            if(porcoes["Arroz"]>0 && porcoes["Carne"]>0 && porcoes["Molho"]>0){
+                porcoes["Arroz"]--;
+                porcoes["Carne"]--;
+                porcoes["Molho"]--;
+            }else{
+                prepararPrato(prato, chef);
+            }
+
+            
         break;
     }
 }
 
 
-var g = Enumerable.Range(1,5).Select(i => Task.Run(() => Garcom(i))).ToList();
-var c = Enumerable.Range(1,3).Select(i => Task.Run(() => Chef(i))).ToList();
+var g = Enumerable.Range(1,2).Select(i => Task.Run(() => Garcom(i))).ToList();
+var c = Enumerable.Range(1,2).Select(i => Task.Run(() => Chef(i))).ToList();
 //var c = Task.Run(Chef);
 
 Task.WaitAll(c.ToArray());
